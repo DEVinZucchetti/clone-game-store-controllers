f545df8abdb28d4f55e5031b2fbdc5895b3b68c6
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-11-17 19:45:25 -0300

    feat: cria as funções do product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index() {
+
+    }
+
+    public function store() {
+
+    }
+
+    public function show() {
+
+    }
+
+    public function destroy() {
+
+    }
+
+    public function update() {
+
+    }
 }
