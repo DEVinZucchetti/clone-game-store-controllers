0072b5b009d93e620952aa8e481e7993e6c6dafa
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2023-11-17 19:56:52 -0300

    feat: sintaxe das funções index e store

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     public function index() {
-
+        $products = Product::all();
+        return $products;
     }
 
-    public function store() {
+    public function store(Request $request) {
+        $data = $request->all();
+
+        $product = Product::create($data);
+
+        return $product;
 
     }
 
