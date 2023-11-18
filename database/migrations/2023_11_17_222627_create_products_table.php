22b929344533bbb86dd32288b1d65ba316a2da11
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-11-17 19:29:00 -0500

    feat: finaliza ex_01

--- database/migrations/2023_11_17_222627_create_products_table.php
+++ database/migrations/2023_11_17_222627_create_products_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name', 150)->unique();
+            $table->decimal('price', 10, 2)->default(0);
+            $table->text('description')->nullable();
             $table->string('cover')->nullable();
-            $table->decimal('price', 10, 2);
-            $table->longText('description');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
+
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-// implemente sua rota;
-
+Route::get('products', [ProductController::class, 'index']);
+Route::post('products', [ProductController::class, 'store']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+Route::delete('products/{id}', [ProductController::class, 'destroy']);
+Route::put('products/{id}', [ProductController::class, 'update']);
