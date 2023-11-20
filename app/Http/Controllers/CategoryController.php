2de744bd0e15d473b7b92ad08d63236f10ea3d56
Caroline Sampaio <caroline_08022@hotmail.com>
2023-11-20 19:25:19 -0300

    feat: adiciona função para cadastro de categoria

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller {
-    public function store() {
+    public function store(Request $request) {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:150|unique:categories',
+            ]);
+
+            $data = $request->all();
+            $category = Category::create($data);
+            return $category;
+        } catch (\Exception $exception) {
+            return response()->json(['message' => $exception->getMessage()], 400);
+        }
     }
 
     public function index() {
