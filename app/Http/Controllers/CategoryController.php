78be4c27ed20dabc1e774c6334169e5762ed9514
Caroline Sampaio <caroline_08022@hotmail.com>
2023-11-20 19:07:36 -0300

    feat: adiciona config model,  rotas e controller

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller {
+    public function store() {
+    }
+
+    public function index() {
+    }
+
+    public function show($id) {
+    }
+
+    public function update($id) {
+    }
+
+    public function destroy($id) {
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,12 @@
 <?php
+
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 // implemente sua rota;
 
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}', [CategoryController::class, 'show']);
+Route::put('/categories/{id}', [CategoryController::class, 'update']);
+Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
