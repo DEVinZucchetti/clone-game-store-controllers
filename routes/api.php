fe04fe2ad9fc46d64e82ea34a36d1ae4c0f00618
Drew <84882983+vdr3w@users.noreply.github.com>
2023-11-22 20:00:59 -0300

    feat(AvaliationRoutes) - Adicionadas rotas para operações CRUD de Avaliation

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AvaliationController;
 
 // implemente sua rota;
 
+Route::post('/avaliations', [AvaliationController::class, 'store']);
+Route::get('/avaliations', [AvaliationController::class, 'index']);
+Route::get('/avaliations/{id}', [AvaliationController::class, 'show']);
+Route::put('/avaliations/{id}', [AvaliationController::class, 'update']);
+Route::delete('/avaliations/{id}', [AvaliationController::class, 'destroy']);
