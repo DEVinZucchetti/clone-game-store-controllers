1816bdbf3ce5d5e89e78aac24621a2f8a90df580
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-11-23 22:38:57 -0500

    feat:cria controller

--- app/Http/Controllers/ProductAssetController.php
+++ app/Http/Controllers/ProductAssetController.php
@@ -1,39 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ProductAsset;
-use Exception;
+
 use Illuminate\Http\Request;
 
 class ProductAssetController extends Controller
 {
-    public function index( Request $request){
-
-        $product_id = $request->query('product_id');
-        
-        $productsAssets = ProductAsset::query()
-        -> where('product_id', $product_id)
-        -> get();
-       
-        return $productsAssets;
-     }
-
- 
-     public function store(Request $request){
-         try {
-             $request->validate([
-                'product_id' => 'required|integer',
-             ]);
- 
-             $data = $request->all();
- 
-             $productAsset = ProductAsset::create($data);
- 
-             return $productAsset;
- 
-         } catch (Exception $exception) {
-             return response()->json(['message' => $exception->getMessage()], 400);
-         }
- 
-     }
+    //
 }
