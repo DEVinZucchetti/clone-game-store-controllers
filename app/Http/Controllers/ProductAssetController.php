666ff4c77f9d38b84af5d43599ea0be9df79d683
Eliana Elizabeth Morillo Taco <eliana_taco@estudante.sesisenai.org.br>
2023-11-23 22:49:55 -0500

    feat:cria metodo list

--- app/Http/Controllers/ProductAssetController.php
+++ app/Http/Controllers/ProductAssetController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ProductAsset;
 use Illuminate\Http\Request;
 
 class ProductAssetController extends Controller
 {
-    //
+    public function index(Request $request){
+
+        $product_id =$request->query('product_id');
+
+        $productsAssets = ProductAsset::query()
+        ->where('product_id', $product_id)
+        ->get();
+        return $productsAssets;
+     }
+ 
 }
